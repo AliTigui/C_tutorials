27315b5d5b080b5ec653e51a8d8145c31d1d68b9
AliTigui <alitighiouart2001@gmail.com>
2024-02-26 23:00:32 +0100

    Structure /type def/ enum

--- tutorial5/main.c
+++ tutorial5/main.c
@@ -1,23 +1,48 @@
 #include<stdio.h>
+#include<string.h>
 /* 
  * in this tutorial we will see
  * how to make structure
  * how to define type
  * how to use structure with pointers
  * */
+ 
+typedef struct Books {
+   char title[50];
+   char author[50];
+   char subject[100];
+   int book_id;
+} Book;
+ 
 struct MyStructure {   // Structure declaration
   int myNum;           // Member (int variable)
   char myLetter;       // Member (char variable)
 };
-
+enum Level {
+  LOW = 25,
+  MEDIUM = 50,
+  HIGH = 75
+};
 int main(){
 
 struct MyStructure s;
-struct myStructure s1 = {13, 'B'};
+enum Level myv=MEDIUM;
 s.myNum=4;
 s.myLetter='C';
 
-printf("structur letter is %c",s.myLetter);
+printf("enum %d",myv);
+
+   Book book;
+ 
+   strcpy( book.title, "C Programming");
+   strcpy( book.author, "Nuha Ali"); 
+   strcpy( book.subject, "C Programming Tutorial");
+   book.book_id = 6495407;
+ 
+   printf( "Book title : %s\n", book.title);
+   printf( "Book author : %s\n", book.author);
+   printf( "Book subject : %s\n", book.subject);
+   printf( "Book book_id : %d\n", book.book_id);
 
 
     return 0;
